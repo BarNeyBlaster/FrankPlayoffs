0ea4512394274029a9f98725d3e28c1562e05a6b
BarNeyBlaster <mcast5283@gmail.com>
2026-08-10 20:07:57 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import base44Compat from "@base44Compat/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -7,7 +6,7 @@ export default defineConfig({
   plugins: [
     base44Compat({
       // Support for legacy code that imports the base44Compat SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44Compat/sdk
+      // can be removed if the code has been updated to use the new SDK imports from /sdk
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
       hmrNotifier: true,
       navigationNotifier: true,
