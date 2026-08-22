9147c8babd5949f56844601ce639720745d027c7
BarNeyBlaster <mcast5283@gmail.com>
2026-08-22 05:10:57 -0400

    Delete src directory

--- src/lib/authReturnTo.js
+++
@@ -1,8 +0,0 @@
-export function getReturnTo() {
-  try {
-    const params = new URLSearchParams(window.location.search);
-    const returnTo = params.get('returnTo');
-    if (returnTo && returnTo.startsWith('/') && !returnTo.startsWith('//')) return returnTo;
-  } catch {}
-  return '/';
-}
